5d7c11432fca21ff10403c2161168dec38bef973
azizi0000-zozo <azizi.mma.adnan@gmail.com>
2020-02-09 18:03:14 +0800

    adding the controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+	protect_from_forgery with: :exception
+	before_filter :categories
+	before_filter :brands
+
+	def categories
+  	@categories = Category.all
+	end
+
+
+  def brands
+  	@brands = []
+  Product.all.each do |product|
+      if @brands.include?(product.brand) == false
+        @brands.push(product.brand)
+      end
+    end
+     return @brands
+	 end
+	 
 end
