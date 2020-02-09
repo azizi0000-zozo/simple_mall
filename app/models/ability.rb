756003832aee256ea31e6a8e3f20c239ce994aff
azizi0000-zozo <azizi.mma.adnan@gmail.com>
2020-02-09 15:34:37 +0800

    Adding the models

---
+++ app/models/ability.rb
@@ -0,0 +1,12 @@
+class Ability < ApplicationRecord
+
+	def initialize(user)
+    # Define abilities for the passed in user here. For example:
+    #
+      user ||= User.new # guest user (not logged in)
+      if user.admin?
+        can :manage, :all
+      else
+        can :read, :all
+      end
+end
